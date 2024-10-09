ce22b06d78759c23697bfeaa625cc180a1f20cdf
user name <user email>
2024-10-09 17:10:18 +0200

    task_4 part 2

--- task_4/main.py
+++ task_4/main.py
@@ -1,26 +1,62 @@
 import re
-
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+def handler(args, contacts, func_name):
+    try:
+        if func_name in ["add", "change", "phone"]:
+            if re.search(r"\d", args[0]):
+                print("The arguments are not correct")
+                return False
+
+            if func_name in ["add", "change"]:
+                if re.search(r"\D", args[1]):
+                    print("The arguments are not correct")
+                    return False
+                
+            if func_name == "phone" and len(args) > 1:
+                print("The arguments are not correct")
+                return False
+            
+        if func_name in ["add"]:
+            if args[0] in contacts:
+                print("This contact already exists")
+                return False
+        
+        if len(args) > 0 and func_name == "all":
+            print("This contact already exists")
+            return False
+        
+        return True
+            
+    except ValueError:
+        print("The arguments are not correct")
+    except IndexError:
+        print("The arguments are not correctr")
+        
+
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added"
+
 def change_contact(args, contacts):
     name, phone = args
     if name in contacts:
+        name, phone = args
         contacts[name] = phone
         return "Contact updated."
     else:
         return "Contact not found"
+    
 def show_phone(arg, contacts):
     if arg[0] in contacts:
         return f"[{contacts[arg[0]]}]"
     else:
         return "Contact not found"
+    
 def show_all(contacts):
     return f"Усі контакти:\n {contacts}"
 
@@ -31,42 +67,26 @@ def main():
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
 
-        try:
-            match command:
-                case "close" | "exit":
-                    print("Good bye!")
-                    break
-                case "hello":
-                    print("How can I help you?")
-                case "add":
-                    if re.search(r"\d", args[0]) or re.search(r"\D", args[1]):
-                        print("The arguments are not correct")
-                    else:
-                        if args[0] in contacts:
-                            print("This contact already exists")
-                        else:
-                            print(add_contact(args, contacts))
-                case "change":
-                    if re.search(r"\d", args[0]) or re.search(r"\D", args[1]):
-                        print("The arguments are not correct")
-                    else:
-                        print(change_contact(args, contacts))
-                case "phone":
-                    if re.search(r"\d", args[0]):
-                        print("The arguments are not correct")
-                    else:
-                        print(show_phone(args, contacts))
-                case "all":
-                    if not args:
-                        print(show_all(contacts))
-                    else:
-                        print('The "all" command must be without arguments.')
-                case _:
-                    print("Invalid command.")
-        except ValueError:
-            print("The arguments are not correct")
-        except IndexError:
-            print("The arguments are not correct")
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                break
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                if handler(args, contacts, command):
+                    print(add_contact(args, contacts))
+            case "change":
+                if handler(args, contacts, command):
+                    print(change_contact(args, contacts))
+            case "phone":
+                if handler(args, contacts, command):
+                    print(show_phone(args, contacts))
+            case "all":
+                if handler(args, contacts, command):
+                    print(show_all(contacts))
+            case _:
+                print("Invalid command.")
 
 if __name__ == "__main__":
     main()
